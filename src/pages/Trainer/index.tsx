af1daeb9a9f0bd8a4a844634bb92e634d5cecaf9
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-09-21 09:56:58 -0300

    implementing timer functionallity

--- src/pages/Trainer/index.tsx
+++ src/pages/Trainer/index.tsx
@@ -1,13 +1,46 @@
 import { TrainerContainer, Timer, Separator, WeekdayButton, ButtonsContainer, RandomDate } from './styles'
-import { useReducer, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { DateGenerator } from '../../utils/DateGenerator'
 import { setFocusWihtKeyboard, clearFocus } from '../../utils/handleKeyboardFocus'
+import { v4 as uuid } from 'uuid'
+import { differenceInMilliseconds } from 'date-fns'
+
+interface Cycle {
+  id: string;
+  startDate: Date;
+  finishDate?: Date;
+}
 
 export function Trainer() {
   const dateGenerator = new DateGenerator()
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [passedMilliseconds, setPassedMilliseconds] = useState<number>(0)
   const [randomDate, setRandomDate] = useState(dateGenerator.generateDate(1700, 2100))
   const weekday = dateGenerator.getWeekday(randomDate)
-  console.log(randomDate, weekday)
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    let interval: number
+
+    if(activeCycle) {
+      interval = setInterval(() => {
+        const currentDifferenceInMilliseconds = differenceInMilliseconds(new Date(), activeCycle.startDate)
+        setPassedMilliseconds(currentDifferenceInMilliseconds)
+      }, 10)
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+
+  }, [passedMilliseconds, setPassedMilliseconds, activeCycle])
+
+  const seconds = Math.floor(passedMilliseconds / 1000)
+  const milliseconds = passedMilliseconds % 1000
+  const paddedSeconds = seconds.toString().padStart(2, '0')
+  const paddedMilliseconds = milliseconds.toString().padStart(3, '0')
 
   function handleUserGuess(data: string) {
     if(data === weekday.toString()) {
@@ -15,7 +48,21 @@ export function Trainer() {
     }
 
     setRandomDate(dateGenerator.generateDate(1700, 2100));
+    createNewCycle()
     clearFocus()
+  }
+
+  function createNewCycle() {
+    const newCycle = {
+      id: uuid(),
+      startDate: new Date(),
+    }
+
+    setCycles((state) => {
+      return [...state, newCycle]
+    })
+    setActiveCycleId(newCycle.id)
+    setPassedMilliseconds(0)
   }
 
   document.addEventListener('keydown', (event) => {
@@ -25,13 +72,13 @@ export function Trainer() {
   return (
     <TrainerContainer>
       <Timer>
-        <span>00</span>
+        <span>{paddedSeconds}</span>
         <Separator>:</Separator>
-        <span>00</span>
+        <span>{paddedMilliseconds}</span>
       </Timer>
 
       <RandomDate>
-        {`${randomDate.day} ${randomDate.month} ${randomDate.year}`}
+        {`${randomDate.day} ${randomDate.month} ${randomDate.year} ${weekday}`}
       </RandomDate>
 
       <ButtonsContainer
